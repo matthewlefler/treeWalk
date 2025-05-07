e2d8a7706427b6254f1c550e2a3c4b853396c0d2
matthewlefler <matthewlefler1@gmail.com>
2025-05-06 20:48:32 -0400

    basic vector3 class

--- src/utils/vector.hpp
+++ src/utils/vector.hpp
@@ -1,11 +1,13 @@
 namespace utils
 {
+    
     class Vector3
     {
         public: 
             float x;
             float y;
             float z;
+
         
             Vector3(float x, float y, float z)
             {
@@ -29,7 +31,43 @@ namespace utils
                     this->z + right.z
                 );
             }
+
+            Vector3 operator-=(Vector3 const& right)
+            {
+                return Vector3(
+                    this->x - right.x, 
+                    this->y - right.y, 
+                    this->z - right.z
+                );
+            }
+
+            Vector3 operator+(Vector3 const& right)
+            {
+                return Vector3(
+                    this->x + right.x, 
+                    this->y + right.y, 
+                    this->z + right.z
+                );
+            }
+
+            Vector3 operator-(Vector3 const& right)
+            {
+                return Vector3(
+                    this->x - right.x, 
+                    this->y - right.y, 
+                    this->z - right.z
+                );
+            }
     };
+
+    static const Vector3 Up = Vector3(0.0f, 1.0f, 0.0f);
+    static const Vector3 Down = Vector3(0.0f, -1.0f, 0.0f);
+    
+    static const Vector3 Left = Vector3(-1.0f, 0.0f, 0.0f);
+    static const Vector3 Right = Vector3(1.0f, 0.0f, 0.0f);
+
+    static const Vector3 Forward = Vector3(0.0f, 0.0f, 1.0f);
+    static const Vector3 Backward = Vector3(0.0f, 0.0f, -1.0f);
 
     struct Vector2
     {
